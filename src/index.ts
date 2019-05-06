c89fc9cc1319146cb9c420f5d1e73097b084889a
Will T <toherwils@gmail.com>
2019-05-05 19:25:19 -0500

    Refactor for hosting

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
+import * as http from "http";
 import * as Koa from "koa";
+import * as logger from "koa-logger";
 import * as kstatic from "koa-static";
 import * as views from "koa-views";
 
@@ -20,73 +22,74 @@ const YEAR = now.getFullYear().toString();
 
 let FINALSLIST: IExamInfo[];
 
-// Use Koa to serve application
-// Koa is like Express but more async
-const app = new Koa();
+function createApp(): (req: any, res: any) => any {
+    // Use Koa to serve application
+    // Koa is like Express but more async
+    const app = new Koa();
 
-// app.use() adds the given function to the chain of functions run on each incoming
-// http request. Koa calls these functions "middleware" (and so does everyone else)
+    // app.use() adds the given function to the chain of functions run on each incoming
+    // http request. Koa calls these functions "middleware" (and so does everyone else)
 
-// Register the static file serving middleware
-// Any files under the ./static subdirectory become available:
-// e.g. ./static/abc.html is served at http://[siteaddress].edu/abc.html
-app.use(kstatic("static"));
+    // Register the logging middleware
+    app.use(logger());
 
-// Register the template rendering middleware
-// This adds the ctx.response.render method, and sets .mst files to be rendered
-// with the Mustache engine
-app.use(views(`${__dirname}/../views`, {
-    map: {mst: "mustache"},
-}));
+    // Register the static file serving middleware
+    // Any files under the ./static subdirectory become available:
+    // e.g. ./static/abc.html is served at http://[siteaddress].edu/abc.html
+    app.use(kstatic("static"));
 
-// Our app logic goes here
-app.use(async (ctx, next) => {
-    if (!FINALSLIST) {
-        // If FINALSLIST is not populated, get finals and filter just the CS courses
-        FINALSLIST = (await getFinals()).filter((xm) => xm.courseDept === "CS");
-    }
+    // Register the template rendering middleware
+    // This adds the ctx.response.render method, and sets .mst files to be rendered
+    // with the Mustache engine
+    const viewDirname = `${__dirname}/../views`;
+    app.use(views(viewDirname, {
+        map: {mst: "mustache"},
+    }));
 
-    // In a larger application, you might use a router here, which connects
-    //  url strings with functions. This is enough for us though.
-    // Split to allow arbitrary query parameters
-    switch (ctx.url.split("?")[0]) {
-        case "/":
-        case "/index.html":
-            // Render index.mst into html, substituting the values in the object into the
-            //  template.
-            await ctx.render("./index.mst", {
-                semester: SEMESTER,
-                year: YEAR,
-                finalsList: FINALSLIST,
-            });
-            break;
-        case "/api/finals.json":
-            ctx.body = {
-                status: "success",
-                finals: FINALSLIST,
-            };
-            break;
-    }
-    // Static asset urls have already been handled earlier in the middleware chain,
-    //  no need for us to test for those here
-});
+    // Our app logic goes here
+    app.use(async (ctx, next) => {
+        if (!FINALSLIST) {
+            // If FINALSLIST is not populated, get finals and filter just the CS courses
+            FINALSLIST = (await getFinals()).filter((xm) => xm.courseDept === "CS");
+        }
 
-app.listen(8080);
-// Equivalent to:
-// http.createServer(app.callback()).listen(8080);
-// In other words, run Koa's function chain in response to an incoming http request on given port
+        // In a larger application, you might use a router here, which connects
+        //  url strings with functions. This is enough for us though.
+        // Split to allow arbitrary query parameters
+        switch (ctx.url.split("?")[0]) {
+            case "/":
+            case "/index.html":
+                // Render index.mst into html, substituting the values in the object
+                //  into the template.
+                await ctx.render("./index.mst", {
+                    semester: SEMESTER,
+                    year: YEAR,
+                    finalsList: FINALSLIST,
+                });
+                break;
+            case "/api/finals":
+                ctx.body = {
+                    status: "success",
+                    finals: FINALSLIST,
+                };
+                break;
+        }
+        // Static asset urls have already been handled earlier in the middleware chain,
+        //  no need for us to test for those here
+    });
 
+    return app.callback();
+}
 
-// Test code
-/*
-getFinals()
-.then((finals) => {
-    console.log(inspect(
-        finals.filter((final) => final.courseDept === "CS"),
-        {showHidden: false, depth: null}),
-    );
-    FINALSLIST = finals;
-}).catch((err) => {
-    console.log(err);
-});
-*/
+if (!process.env.IS_NOW) {
+    // This block is just for my deployment - the below code will always run in local development
+
+    http.createServer(createApp()).listen(8080);
+
+    // app.listen(8080);
+    // Equivalent to:
+    // http.createServer(app.callback()).listen(8080);
+    // In other words, run Koa's function chain in response to an incoming http request on given port
+}
+
+export default (createApp());
